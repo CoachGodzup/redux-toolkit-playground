576cb152523172dfa05df900e7a2b0176f3749a4
Matteo Garza <matteo.garza@facile.it>
2022-04-04 12:57:33 +0200

    initial commit

---
+++ src/components/ActionPanel/ActionPanel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ActionPanel.css';
+
+const ActionPanel = () => (
+  <div className='actionPanel'>
+    <button>🔼 Buy</button>
+    <button>🔽 Sell</button>
+    <button>🔁 Reset</button>
+  </div>
+);
+
+export default ActionPanel;
---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Counter = () => <div>You bought some 🍎</div>;
+
+export default Counter;
