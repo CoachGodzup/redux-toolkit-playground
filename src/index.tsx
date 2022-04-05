16a61017407d04696e9388253065895c54e7c3aa
Matteo Garza <matteo.garza@facile.it>
2022-04-05 09:23:24 +0200

    change render method from react 17 to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
